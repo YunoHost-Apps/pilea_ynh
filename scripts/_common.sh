4b3946bda91294a8afd0d8af1044e491db8ef348
Simon Mellerin <simon.mellerin@makina-corpus.com>
2018-10-29 14:07:06 +0100

    Use composer + update script improvements

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,6 @@
-#
-# Common variables
-#
+# =============================================================================
+# COMMON VARIABLES
+# =============================================================================
 
 # Package dependencies
 PKG_DEPENDENCIES="php5-cli php5-mysql php5-json php5-gd php5-tidy php5-curl php-gettext"
@@ -18,6 +18,10 @@ exec_as() {
     sudo -u "$USER" "$@"
   fi
 }
+
+# =============================================================================
+# COMMON PILEA FUNCTIONS
+# =============================================================================
 
 # Execute a command through the Pilea console
 # usage: exec_console AS_USER WORKDIR COMMAND [ARG ...]
@@ -47,6 +51,36 @@ CHECK_SIZE () {	# Check if enough disk space available on backup storage
     WARNING echo "Space available: $(HUMAN_SIZE $free_space)"
     ynh_die "Space needed: $(HUMAN_SIZE $backup_size)"
   fi
+}
+
+# Execute a composer command from a given directory
+# usage: composer_exec workdir COMMAND [ARG ...]
+exec_composer() {
+  local workdir=$1
+  shift 1
+
+  COMPOSER_HOME="${workdir}/.composer" \
+    php "${workdir}/composer.phar" $@ \
+      -d "${workdir}" --quiet --no-interaction
+}
+
+# Install and initialize Composer in the given directory
+# usage: init_composer destdir
+init_composer() {
+  local destdir=$1
+
+  # install composer
+  curl -sS https://getcomposer.org/installer \
+    | COMPOSER_HOME="${destdir}/.composer" \
+        php -- --quiet --install-dir="$destdir" \
+    || ynh_die "Unable to install Composer"
+
+  # install composer.json
+  cp "${destdir}/composer.json-dist" "${destdir}/composer.json"
+
+  # update dependencies to create composer.lock
+  exec_composer "$destdir" install --no-dev \
+    || ynh_die "Unable to install Pilea core dependencies"
 }
 
 
