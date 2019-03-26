1b67e04bc86409a03596b0cb5c9ff495185aec0a
Simon Mellerin <simon.mellerin@makina-corpus.com>
2019-03-26 13:13:08 +0100

    Fix php 7.2 install method

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -90,27 +90,6 @@ ynh_delete_file_checksum () {
 # PHP7.2 helpers
 #
 
-ynh_install_php7 () {
-
-  ynh_package_update
-  ynh_package_install apt-transport-https --no-install-recommends
-
-  wget -q -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-  echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php7.list
-
-  ynh_package_update
-  ynh_install_app_dependencies php7.2 php7.2-zip php7.2-fpm php7.2-mysql php7.2-xml php7.2-intl php7.2-mbstring php7.2-gd php7.2-curl php7.2-bcmath php7.2-opcache php7.2-sqlite3
-
-  sudo update-alternatives --install /usr/bin/php php /usr/bin/php5 70
-}
-
-ynh_remove_php7 () {
-  sudo rm -f /etc/apt/sources.list.d/php7.list
-  sudo apt-key del 4096R/89DF5277
-  sudo apt-key del 2048R/11A06851
-  ynh_remove_app_dependencies php7.2 php7.2-zip php7.2-fpm php7.2-mysql php7.2-xml php7.2-intl php7.2-mbstring php7.2-gd php7.2-curl php7.2-bcmath php7.2-opcache php7.2-sqlite3
-}
-
 ynh_add_fpm7.2_config () {
   # Configure PHP-FPM 7.2 by default
   local fpm_config_dir="/etc/php/7.2/fpm"
