7776d1f56cb1a57c28dfc82c4d3c0ea94528fc28
Simon Mellerin <simon.mellerin@makina-corpus.com>
2019-04-01 13:31:46 +0200

    try to fix php dependcies again & again

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,6 @@
 # dependencies used by the app
-pkg_dependencies="apt-transport-https"
-php_dependencies="php7.2-zip php7.2-fpm php7.2-mysql php7.2-xml php7.2-intl php7.2-mbstring php7.2-gd php7.2-curl php7.2-bcmath php7.2-opcache php7.2-sqlite3"
+php_install_dependencies="apt-transport-https"
+pkg_dependencies="php7.2-zip php7.2-fpm php7.2-mysql php7.2-xml php7.2-intl php7.2-mbstring php7.2-gd php7.2-curl php7.2-bcmath php7.2-opcache php7.2-sqlite3"
 
 # Execute a command as another user
 # usage: exec_as USER COMMAND [ARG ...]
