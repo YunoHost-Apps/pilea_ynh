50c6578aaa572158181a139d7cd5a8362b778892
Simon Mellerin <simon.mellerin@makina-corpus.com>
2019-08-30 11:30:44 +0200

    Add php dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,4 +1,4 @@
 # dependencies used by the app
 pkg_dependencies="apt-transport-https"
 php_app_version="7.3"
-php_additonal_packages="php7.3-zip php7.3-mysql php7.3-xml php7.3-intl php7.3-mbstring php7.3-gd php7.3-curl php7.3-bcmath php7.3-opcache php7.3-sqlite3"
+php_additonal_packages="php7.3-fpm php7.3-cli php7.3-zip php7.3-mysql php7.3-xml php7.3-intl php7.3-mbstring php7.3-gd php7.3-curl php7.3-bcmath php7.3-opcache php7.3-sqlite3"
